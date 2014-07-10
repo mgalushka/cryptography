3faf0efd53f28232224ed89fad205fde7858210f
Maxim Galushka <maxim.galushka@mail.ru>
2014-07-10 16:51:50 +0300

    corrected

--- src/com/maximgalushka/cryptography/week2/AES_CTR_Mode.java
+++ src/com/maximgalushka/cryptography/week2/AES_CTR_Mode.java
@@ -34,7 +34,7 @@ public class AES_CTR_Mode {
         int i = 1;
         while (16 * i < N) {
             byte[] encrypted = new byte[16];
-            System.arraycopy(full, 16 * i, encrypted, 0, Math.min(16, full.length-16*i));
+            System.arraycopy(full, 16 * i, encrypted, 0, Math.min(16, full.length - 16 * i));
             i++;
 
             byte[] first = cipher.doFinal(IV);
@@ -53,14 +53,16 @@ public class AES_CTR_Mode {
     public static void main(String[] args) throws NoSuchPaddingException, NoSuchAlgorithmException,
             IllegalBlockSizeException, BadPaddingException, NoSuchProviderException, InvalidKeyException {
 
-        //String T = "69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329";
-        //String key = "36f18357be4dbd77f050515c73fcf9f2";
+        String T1 = "69dda8455c7dd4254bf353b773304eec0ec7702330098ce7f7520d1cbbb20fc388d1b0adb5054dbd7370849dbf0b88d393f252e764f1f5f7ad97ef79d59ce29f5f51eeca32eabedd9afa9329";
+        String key1 = "36f18357be4dbd77f050515c73fcf9f2";
 
-        String key = "36f18357be4dbd77f050515c73fcf9f2";
-        String T = "770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451";
+        String T2 = "770b80259ec33beb2561358a9f2dc617e46218c0a53cbeca695ae45faa8952aa0e311bde9d4e01726d3184c34451";
+        String key2 = "36f18357be4dbd77f050515c73fcf9f2";
 
-        AES_CTR_Mode m = new AES_CTR_Mode(key);
+        AES_CTR_Mode m = new AES_CTR_Mode(key1);
+        System.out.println(m.decrypt(T1));
 
-        System.out.println(m.decrypt(T));
+        m = new AES_CTR_Mode(key2);
+        System.out.println(m.decrypt(T2));
     }
 }
